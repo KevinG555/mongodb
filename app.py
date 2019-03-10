5df37089ca4bb4c6d55b37d5a348c0893ae5e18c
Kevin Gurol <cagataygurol@gmail.com>
2019-03-10 16:30:30 +0000

    index.html added

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'my_recipe'
+app.config["MONGO_URI"] = 'mongodb://kegrl:Cagla2019!@ds263295.mlab.com:63295/my_recipe'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello again'
-
-
+@app.route('/get_recipes')          
+def get_recipes():             
+    return render_template("index.html", recipes=mongo.db.recipes.find())
+        
+            
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
