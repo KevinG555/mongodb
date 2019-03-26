c6d51a0deb7e38dec68a0f63366945993c695163
Kevin Gurol <cagataygurol@gmail.com>
2019-03-26 22:58:04 +0000

    suggestions.html added

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ mongo = PyMongo(app)
 @app.route('/get_recipes')          
 def get_recipes():             
     return render_template("index.html", recipes=mongo.db.recipes.find())
+
+@app.route('/get_suggestion')          
+def get_suggestion():             
+    return render_template("suggestions.html")   
+    
     
 @app.route("/sandwiches")
 def sandwiches():
@@ -35,13 +40,37 @@ def vegan():
 def add_recipe():
     return render_template("addrecipe.html", categories = mongo.db.categories.find())
     
-@app.route("/insert_recipe", methods=['POST'])
+@app.route("/edit_recipe", methods=['GET'])
 def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('get_recipes'))
+    return redirect(url_for('edit_recipe'))
     
-
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe,
+                           categories=all_categories)    
+    
+@app.route('/update_recipe/<recipe_id>', methods=["POST", ])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'recipe_name':request.form.get('recipe_name'),
+        'category_name':request.form.get('category_name'),
+        'recipe_ingredients': request.form.get('recipe_ingredients'),
+        'recipe_direction': request.form.get('recipe_direction'),
+        'recipe_preptime':request.form.get('recipe_preptime'),
+        'recipe_cook':request.form.get('recipe_cook'),
+        'recipe_person':request.form.get('recipe_person'),
+        'recipe_image_url':request.form.get('recipe_image_url')
+        
+        
+        
+    })
+    return redirect(url_for('get_recipes'))
     
 
 if __name__ == '__main__':
